777a30fb0f2e59a6f997ec6fbde5f9f0cfbfc472
nicolecht <nicolecht@hotmail.com>
2022-07-29 18:34:16 +0800

    update js

--- script.js
+++ script.js
@@ -3,27 +3,68 @@ taskBox = document.querySelector(".taskbox");
 
 let todos = JSON.parse(localStorage.getItem("todo-list")); // get localStorage todo-list
 
+// show all todos
 function showTodo() {
     let li = "";
-    todos.forEach((todo, id) => {
-        li += `<li class="task">
-                    <label for="${id}">
-                        <input type="checkbox" name="" id="${id}">
-                        <p>${todo.name}</p>
-                    </label>
-                    <div class="settings">
-                        <i class="uil-ellipsis-h"></i>
-                        <ul class="task-menu">
-                            <li><i class="uil-pen"></i>Edit</li>
-                            <li><i class="uil-trash"></i>Delete</li>
-                        </ul>
-                    </div>
-                </li>`;
-    });
+    if(todos) {
+        todos.forEach((todo, id) => {
+            // if todo status is completed, set isCompleted value to checked
+            let isCompleted = todo.status == "completed" ? "checked" : "";
+            li += `<li class="task">
+                        <label for="${id}">
+                            <input onclick="updateStatus(this)" type="checkbox" name="" id="${id}" ${isCompleted}>
+                            <p class="${isCompleted}">${todo.name}</p>
+                        </label>
+                        <div class="settings">
+                            <i onclick="showMenu(this)" class="uil-ellipsis-h"></i>
+                            <ul class="task-menu">
+                                <li><i class="uil-pen"></i>Edit</li>
+                                <li onclick="deleteTask(${id})"><i class="uil-trash"></i>Delete</li>
+                            </ul>
+                        </div>
+                    </li>`;
+        });
+    }
     taskBox.innerHTML = li;
 }
 showTodo();
 
+// show task-menu
+function showMenu(selectedTask) {
+    // get task-menu ul
+    let taskMenu = selectedTask.parentElement.lastElementChild;
+    taskMenu.classList.add("show");
+    document.addEventListener("click", e => {
+        // remove show class from task-menu on the doc click
+        if(e.target.tagName != "I" || e.target != selectedTask) {
+            taskMenu.classList.remove("show");
+        }
+    })
+}
+
+// delete task
+function deleteTask(deleteId) {
+    // remove selected task from array/todos
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos)); // update storage
+    showTodo();
+}
+
+// update task status
+function updateStatus(selectedTask) {
+    // get paragraph containing task name
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed"; //update to complete status
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending"; //update to pending status
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos)); // save new status to storage
+}
+
+// add new task
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
     if(e.key == "Enter" && userTask) {
